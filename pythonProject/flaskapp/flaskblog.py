590a91885ea16d2e20912bde6818ab76edfa8d1e
Purkayastha, Chandreyee <chandreyee.purkayastha@intel.com>
2024-08-26 11:03:44 +0530

    Added templates for home and about page

--- pythonProject/flaskapp/flaskblog.py
+++ pythonProject/flaskapp/flaskblog.py
@@ -1,15 +1,15 @@
-from flask import Flask #from flask module import Flask class
+from flask import Flask,render_template #from flask module import Flask class
 
 app = Flask(__name__) #create an object of flask class
 
 @app.route("/")  #root dir
-@app.route("/home")
+@app.route("/home") #home
 def home():
-    return "<h1>hello world</h1>"
+    return render_template('home.html')
 
 @app.route("/about") #adding an about route
 def about():
-    return "<h5>About Page</h5>"
+    return render_template('about.html')
 
 if __name__ == '__main__': #if it is run directly and not if imported onto other py file
-    app.run(debug=True) #debug=True because to not restart the web server each time we make a change akshita
+    app.run(debug=True) #debug=True because to not restart the web server each time we make a change
