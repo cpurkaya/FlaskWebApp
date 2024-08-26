47a3e11569d0c5a876f97f3452e6ed6a1df0c7b6
Purkayastha, Chandreyee <chandreyee.purkayastha@intel.com>
2024-08-26 15:49:14 +0530

    Added login and registration form in the login and registration UI

--- pythonProject/flaskapp/flaskblog.py
+++ pythonProject/flaskapp/flaskblog.py
@@ -4,9 +4,12 @@ Flask is a micro web framework.
 The templating engine that flask uses is called jinja2
 '''
 
-from flask import Flask,render_template #from flask module import Flask class
+from flask import Flask, render_template, url_for, flash, redirect #from flask module import Flask class
+from forms import RegistrationForm, LoginForm
+
 
 app = Flask(__name__) #create an object of flask class
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 data=[
         {
@@ -30,6 +33,27 @@ def home():
 @app.route("/about") #adding an about route
 def about():
     return render_template('about.html',title='About')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
 
 if __name__ == '__main__': #if it is run directly and not if imported onto other py file
     app.run(debug=True) #debug=True because to not restart the web server each time we make a change
