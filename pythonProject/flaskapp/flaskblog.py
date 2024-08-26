13b4c8321654f17654537cc3f2933e44e9131b4a
Purkayastha, Chandreyee <chandreyee.purkayastha@intel.com>
2024-08-26 11:35:29 +0530

    Added for loop

--- pythonProject/flaskapp/flaskblog.py
+++ pythonProject/flaskapp/flaskblog.py
@@ -1,11 +1,31 @@
+'''
+This is a demo project of flask implementation.
+Flask is a micro web framework.
+The templating engine that flask uses is called jinja2
+'''
+
 from flask import Flask,render_template #from flask module import Flask class
 
 app = Flask(__name__) #create an object of flask class
 
+data=[
+        {
+            'title': 'First post',
+            'content': 'First post content line 1',
+            'date_posted': '24-08-2024',
+            'author': 'Chandreyee Purkayastha'
+        },
+        {
+            'title': 'Second post',
+            'content': 'Second post content line 1',
+            'date_posted': '25-08-2024',
+            'author': 'Dummy User 2'
+        }
+]
 @app.route("/")  #root dir
 @app.route("/home") #home
 def home():
-    return render_template('home.html')
+    return render_template('home.html',post=data)
 
 @app.route("/about") #adding an about route
 def about():
